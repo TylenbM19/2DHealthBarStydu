5d658e02b05f5f0740c686ca11cc422ae0bb7a26
Igor <e-e-dsf-dsf@yandex.ru>
2022-07-12 09:47:30 +0300

    123

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 { 
     [SerializeField] private int _maxHealth = 10;
     [SerializeField] private int _currentHealth;
+    [SerializeField] private Button _damageButton;
+    [SerializeField] private Button _healingButton;
+    [SerializeField] private int _damage;
+    [SerializeField] private int _healing;
 
     public event UnityAction<int, int> OnHealthChanged;
     private int _minHealth = 0;
@@ -33,5 +38,26 @@ public class Player : MonoBehaviour
             _currentHealth = tempCurrentHealth;
             OnHealthChanged?.Invoke(_currentHealth, _maxHealth);
         }
+    }
+    private void OnEnable()
+    {
+        _damageButton.onClick.AddListener(OnDamageButtonClick);
+        _healingButton.onClick.AddListener(OnHealtingButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _damageButton.onClick.RemoveListener(OnDamageButtonClick);
+        _healingButton.onClick.RemoveListener(OnHealtingButtonClick);
+    }
+
+    private void OnDamageButtonClick()
+    {
+        ChangeHealth(-_damage);
+    }
+
+    private void OnHealtingButtonClick()
+    {
+        ChangeHealth(_healing);
     }
 }
